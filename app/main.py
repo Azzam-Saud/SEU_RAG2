0100747fb3f69f9d44fea9055a4d99cc1f2f04f0
Azzam-Saud <144704235+Azzam-Saud@users.noreply.github.com>
2025-12-31 12:00:41 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,21 +2,29 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
-
 from app.rag_engine import rag_llm_answer
 
-app = FastAPI(title="SEU chatbot API")
+app = FastAPI(title="SEU RAG API")
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+
 class Query(BaseModel):
     question: str
+
 
 @app.get("/", response_class=HTMLResponse)
 def home():
     with open("static/index.html", encoding="utf-8") as f:
         return f.read()
 
+
 @app.post("/ask")
 def ask(q: Query):
-    return {"answer": rag_llm_answer(q.question)}
+    try:
+        return {"answer": rag_llm_answer(q.question)}
+    except Exception as e:
+        return {
+            "answer": "❌ خطأ داخلي",
+            "debug": str(e)
+        }
