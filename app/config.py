c3f62d9c66b5ad1675a8e146824def9118ee71c6
Azzam-Saud <144704235+Azzam-Saud@users.noreply.github.com>
2025-12-31 10:51:54 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,14 +1,6 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-
-PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
-PINECONE_INDEX_NAME = "seu-ragbot"
-PINECONE_ENV = "us-east-1"
 if not OPENAI_API_KEY:
-    raise RuntimeError("OPENAI_API_KEY is not set")
-if not PINECONE_API_KEY:
-    raise RuntimeError("PINECONE_API_KEY is not set")
+    raise ValueError("OPENAI_API_KEY is not set")
